3e07278fa0056aa9e5b147e8d14350dc70dc1001
01luisfonseca <01luisfonseca@gmail.com>
2019-07-06 12:22:35 -0500

    Ajuste de formato

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 let list = [];
 
 createdObject(elm => {
-   console.log(elm, list) 
-   list.push(elm)
-   drawList(list) 
-})
+  console.log(elm, list);
+  list.push(elm);
+  drawList(list);
+});
 
-updateObject((elm) =>{  
-   console.log(list, elm)
-   var index = list.findIndex(function(element){
-      return elm.id === element.id;
-   });
-   console.log(index)
-   list.pop(list)
-   list.splice(index,0,elm)
-   drawList(list)
-})
+updateObject(elm => {
+  console.log(list, elm);
+  var index = list.findIndex(function(element) {
+    return elm.id === element.id;
+  });
+  console.log(index);
+  list.pop(list);
+  list.splice(index, 0, elm);
+  drawList(list);
+});
 
-deleteObject((elm) =>{ 
-    console.log(elm,list)
-    var index = list.findIndex(function(element){
-        return elm.id === element.id;
-    });
-    console.log(index)
-    list.splice(index,1)
-    drawList(list)
-})
+deleteObject(elm => {
+  console.log(elm, list);
+  var index = list.findIndex(function(element) {
+    return elm.id === element.id;
+  });
+  console.log(index);
+  list.splice(index, 1);
+  drawList(list);
+});
 
 const myvar = [
-	null,
-	undefined,
-	function(omg){
-		return console.log("Esto es omg", omg)
-	},
-	{
-		id: elm => console.log("Es un elm", elm)
-	},
-	" Es un texto para jugar"
-]
+  null,
+  undefined,
+  function(omg) {
+    return console.log("Esto es omg", omg);
+  },
+  {
+    id: elm => console.log("Es un elm", elm)
+  },
+  " Es un texto para jugar"
+];
 
 const myvar2 = {
-	uno: "Es uno",
-	"dos y tres": "DosyTres"
+  uno: "Es uno",
+  "dos y tres": "DosyTres"
+};
+
+function texto() {
+  return "testo para";
 }
 
-function texto () {
-	return "testo para"
-}
-
-console.log(myvar)
+console.log(myvar);
